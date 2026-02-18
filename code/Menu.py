ac79bef83c3576995be5c5afae45397ca984857a
RafaelRPC <rafaelrubensas@gmail.com>
2026-02-18 12:18:00 -0300

    Pause - AP02

--- code/Menu.py
+++ code/Menu.py
@@ -1,5 +1,11 @@
+import pygame.image
+
 class Menu:
     def __init__(self, in_window):
         self.window = in_window
+        self.surf = pygame.image.load('')
+        self.rect = self.surf.get_rect(left=0, top=0)
     def run(self,):
+        self.window.blit(source=self.surf, dest=self.rect)
+        pygame.display.flip()
         pass
